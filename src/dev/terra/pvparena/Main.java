27f243ac6936b801bd05e3d9c7f9ef6bb663203c
MICAIAHSPC\cake <micjon89@gmail.com>
2021-04-06 14:58:04 -0400

    Added enter pvp arena

--- src/dev/terra/pvparena/Main.java
+++ src/dev/terra/pvparena/Main.java
@@ -1,5 +1,8 @@
 package dev.terra.pvparena;
 
+import dev.terra.pvparena.events.welcomemessage;
+import org.bukkit.ChatColor;
+import dev.terra.pvparena.events.enterarena;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {

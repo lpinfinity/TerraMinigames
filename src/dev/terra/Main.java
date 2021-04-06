47b2a5cc2a1fa01e1f597662a58d8d853dc24482
BuildTools <unconfigured@null.spigotmc.org>
2021-04-05 20:12:46 -0400

    set up basics

--- src/dev/terra/Main.java
+++ src/dev/terra/Main.java
@@ -1,4 +1,17 @@
 package dev.terra;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("TerraPVPArena has started");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("TerraPVPArena has stopped");
+    }
+
 }

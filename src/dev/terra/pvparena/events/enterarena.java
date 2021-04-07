9328fae96d2b0de7b548f15281507b430adeefc0
MICAIAHSPC\cake <micjon89@gmail.com>
2021-04-06 22:54:47 -0400

    update

--- src/dev/terra/pvparena/events/enterarena.java
+++ src/dev/terra/pvparena/events/enterarena.java
@@ -2,10 +2,12 @@ package dev.terra.pvparena.events;
 
 import org.bukkit.Bukkit;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class enterarena implements Listener {
 
@@ -20,7 +22,7 @@ public class enterarena implements Listener {
 
         if (x > 16 && x < 66) {
             if (z > -26 && z <33) {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "give " + player.getName() + " wooden_sword");
+                player.getInventory().setItem(8, new ItemStack(Material.WOODEN_SWORD,1));
             }
         }
 

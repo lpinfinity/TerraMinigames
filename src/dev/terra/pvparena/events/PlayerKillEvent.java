c4878f0ec30b7e69e3b9367bf18707163313d416
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 23:05:52 -0400

    Setup config for PlayerKillEvent

--- src/dev/terra/pvparena/events/PlayerKillEvent.java
+++ src/dev/terra/pvparena/events/PlayerKillEvent.java
@@ -1,7 +1,10 @@
 package dev.terra.pvparena.events;
 
 import dev.terra.pvparena.Main;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PlayerKillEvent implements Listener {
 
@@ -10,4 +13,12 @@ public class PlayerKillEvent implements Listener {
     public PlayerKillEvent(Main main) {
         this.main = main;
     }
+
+    @EventHandler
+    public void onPlayerKill(PlayerDeathEvent event) {
+        if(event.getEntityType().equals(EntityType.PLAYER)) {
+            main.config.set(event.getEntity().getUniqueId().toString(), main.config.get(event.getEntity().getUniqueId().toString() + 1));
+        }
+    }
+
 }

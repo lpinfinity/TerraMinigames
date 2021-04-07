d8d54ad727d9164c859b981218a50920dac1ae1b
MICAIAHSPC\cake <micjon89@gmail.com>
2021-04-06 23:44:42 -0400

    update

--- src/dev/terra/pvparena/events/clearinventory.java
+++ src/dev/terra/pvparena/events/clearinventory.java
@@ -1,10 +1,19 @@
 package dev.terra.pvparena.events;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 
 public class clearinventory implements Listener {
 
+    @EventHandler
+    public static void onPlayerDeath(PlayerDeathEvent event) {
+
+        onPlayerDeath("clearinventory");
+
+    }
 
 
 }

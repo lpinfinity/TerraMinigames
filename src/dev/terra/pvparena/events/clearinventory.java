587177da562461d920eb6296d3e2acd03c6515c2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 23:47:19 -0400

    Setup config for PlayerKillEvent

--- src/dev/terra/pvparena/events/clearinventory.java
+++ src/dev/terra/pvparena/events/clearinventory.java
@@ -5,13 +5,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import java.util.Objects;
+
 
 public class clearinventory implements Listener {
 
     @EventHandler
     public static void onPlayerDeath(PlayerDeathEvent event) {
 
-        onPlayerDeath("clearinventory");
+        Objects.requireNonNull(event.getEntity().getPlayer()).getInventory().clear();
 
     }
 

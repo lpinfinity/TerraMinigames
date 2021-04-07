db01efd64bb1d6409d572c7e25ad40325ccec1ae
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 22:13:46 -0400

    no message

--- src/dev/terra/pvparena/Main.java
+++ src/dev/terra/pvparena/Main.java
@@ -1,17 +1,45 @@
 package dev.terra.pvparena;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends JavaPlugin {
 
+    private File totalKillsFile;
+    private FileConfiguration totalKills;
+
     @Override
     public void onEnable() {
+        createTotalKills();
         System.out.println("TerraPVPArena has started");
     }
 
     @Override
     public void onDisable() {
         System.out.println("TerraPVPArena has stopped");
+    }
+
+    public FileConfiguration getTotalKills() {
+        return this.totalKills;
+    }
+
+    private void createTotalKills() {
+        totalKillsFile = new File(getDataFolder(), "totalKills.yml");
+        if(!totalKillsFile.exists()) {
+            totalKillsFile.getParentFile().mkdirs();
+            saveResource("totalKills.yml", false);
+        }
+        totalKills = new YamlConfiguration();
+        try {
+            totalKills.load(totalKillsFile);
+        }catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
 }

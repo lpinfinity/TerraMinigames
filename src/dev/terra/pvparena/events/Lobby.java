d8be8f204f832494ed8a7c1bdd4c0832e99b7ef3
MICAIAHSPC\cake <micjon89@gmail.com>
2021-04-07 00:07:11 -0400

    update

--- src/dev/terra/pvparena/events/Lobby.java
+++ src/dev/terra/pvparena/events/Lobby.java
@@ -17,7 +17,7 @@ public class Lobby implements Listener {
         int y = player.getLocation().getBlockY();
         int z = player.getLocation().getBlockZ();
 
-        if (x > -30 && x < 16) {
+        if (x > -30 && x < 14) {
             if (z > -50 && z < 50) {
 
                 Objects.requireNonNull(event.getPlayer()).getInventory().clear();

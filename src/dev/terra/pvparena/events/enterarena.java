6318601d27ff26dd72dbd4507bc6e146ff116a97
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 22:29:43 -0400

    no message

---
+++ src/dev/terra/pvparena/events/enterarena.java
@@ -0,0 +1,28 @@
+package dev.terra.pvparena.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class enterarena implements Listener {
+
+    @EventHandler
+    public static void onPlayerWalk(PlayerMoveEvent event) {
+
+        Player player = event.getPlayer();
+        int x = player.getLocation().getBlockX();
+        int y = player.getLocation().getBlockY();
+        int z = player.getLocation().getBlockZ();
+
+        if (x > 16 && x < 66) {
+            if (z > -26 && z <33) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "give " + player.getName() + " wooden_sword");
+            }
+        }
+
+
+    }
+}
---
+++ src/dev/terra/pvparena/events/welcomemessage.java
@@ -0,0 +1,23 @@
+package dev.terra.pvparena.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.net.http.WebSocket;
+
+public class welcomemessage implements Listener {
+
+    @EventHandler
+    public static void  onPlayerJoin(PlayerJoinEvent event) {
+
+        Player player = event.getPlayer();
+        player.sendMessage(ChatColor.GREEN + "Welcome to the Terra Minegames Server" );
+        player.sendMessage(ChatColor.GREEN + "Step on pressure plates next to Minegames to join" );
+        player.sendMessage(ChatColor.GREEN + "Do /hub to Teleport to the hub" );
+
+    }
+
+}

c7136b24e5198f5d6b474f52f0d7e44dc4e92205
MICAIAHSPC\cake <micjon89@gmail.com>
2021-04-06 22:47:36 -0400

    no message

--- src/dev/terra/pvparena/events/enterarena.java
+++ src/dev/terra/pvparena/events/enterarena.java
@@ -27,24 +27,4 @@ public class enterarena implements Listener {
 
     }
 }
-<<<<<<< HEAD
-=======
-=======
 
-
-            Player player = event.getPlayer();
-            int x = player.getLocation().getBlockX();
-            int y = player.getLocation().getBlockY();
-            int z = player.getLocation().getBlockZ();
-
-            if (x > 16 && x < 66) {
-                if (z > -26 && z <33) {
-                    player.getInventory().setItem(0, new ItemStack(Material.WOODEN_SWORD, 1));
-                }
-            }
-
-        }
-
-    }
->>>>>>> 0341b8e7c5b10864d77944c4db3bc1bd92b539a1
->>>>>>> d92fdbba42c289abab4bf9da57c23ea321a79695

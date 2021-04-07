865bef06cc68e58ced9b0db57f4c2523bd4bc6af
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 22:50:59 -0400

    removed unused code, registered playerkilled event

--- src/dev/terra/pvparena/Main.java
+++ src/dev/terra/pvparena/Main.java
@@ -1,31 +1,18 @@
 package dev.terra.pvparena;
 
-
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-
+import dev.terra.pvparena.events.PlayerKillEvent;
 import dev.terra.pvparena.events.welcomemessage;
-import org.bukkit.ChatColor;
 import dev.terra.pvparena.events.enterarena;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Main extends JavaPlugin {
-
-    private File totalKillsFile;
-    private FileConfiguration totalKills;
 
     @Override
     public void onEnable() {
 
         getServer().getPluginManager().registerEvents(new enterarena(),this);
         getServer().getPluginManager().registerEvents(new welcomemessage(), this);
-
-        createTotalKills();
+        getServer().getPluginManager().registerEvents(new PlayerKillEvent(), this);
 
         System.out.println("TerraPVPArena has started");
     }
@@ -33,24 +20,6 @@ public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         System.out.println("TerraPVPArena has stopped");
-    }
-
-    public FileConfiguration getTotalKills() {
-        return this.totalKills;
-    }
-
-    private void createTotalKills() {
-        totalKillsFile = new File(getDataFolder(), "totalKills.yml");
-        if(!totalKillsFile.exists()) {
-            totalKillsFile.getParentFile().mkdirs();
-            saveResource("totalKills.yml", false);
-        }
-        totalKills = new YamlConfiguration();
-        try {
-            totalKills.load(totalKillsFile);
-        }catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
     }
 
 }

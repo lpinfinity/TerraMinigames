a3f2602c63fa916fa91d51582971096fe17f0393
MICAIAHSPC\cake <micjon89@gmail.com>
2021-04-07 00:04:50 -0400

    update

--- src/dev/terra/pvparena/events/Lobby.java
+++ src/dev/terra/pvparena/events/Lobby.java
@@ -1,9 +1,34 @@
 package dev.terra.pvparena.events;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.Objects;
 
 public class Lobby implements Listener {
 
+    @EventHandler
+    public static void onPlayerWalk(PlayerMoveEvent event) {
 
+        Player player = event.getPlayer();
+        int x = player.getLocation().getBlockX();
+        int y = player.getLocation().getBlockY();
+        int z = player.getLocation().getBlockZ();
+
+        if (x > -30 && x < 16) {
+            if (z > -50 && z < 50) {
+
+                Objects.requireNonNull(event.getPlayer()).getInventory().clear();
+            }
+
+
+        }
+
+    }
 
 }
+
+
+

00ace3c7c34b5926074048379df938f6c00df0bd
MICAIAHSPC\cake <micjon89@gmail.com>
2021-04-07 23:24:58 -0400

    update

--- src/dev/terra/pvparena/events/Lobby.java
+++ src/dev/terra/pvparena/events/Lobby.java
@@ -1,8 +1,10 @@
 package dev.terra.pvparena.events;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.Objects;
@@ -10,9 +12,20 @@ import java.util.Objects;
 public class Lobby implements Listener {
 
     @EventHandler
-    public static void onPlayerWalk(PlayerMoveEvent event) {
+    public static void onPlayerDamage(EntityDamageEvent event) {
 
+        if (event.getEntity() instanceof Player) {
 
+            Player player = (Player) event.getEntity();
+            if (player.isFlying() || player.getAllowFlight()) {
+
+                player.setFlying(false);
+                player.setAllowFlight(false);
+                player.setFallDistance(0);
+
+            }
+
+        }
 
     }
 }
